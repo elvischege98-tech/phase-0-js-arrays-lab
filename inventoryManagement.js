19b429c5925ca6f9e174fab2595629824903446d
Elvis Chege <elvischege98@gmail.com>
2026-03-20 23:39:53 +0300

    Added functionality

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,6 +1,24 @@
 // Write your code here
 
+let products =[`Laptop`, `Phone`, `Headphones`, `Monitor`];
 
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(product) {
+  products.push(product);
+}
+
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  }
+}
+
+function removeLastProduct() {
+  products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
