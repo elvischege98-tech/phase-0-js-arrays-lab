c92d6c4fea20f303f2ecd9a12d5f2be56f0a758d
Elvis Chege <elvischege98@gmail.com>
2026-03-23 11:46:34 +0300

    Finished array lab

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -2,23 +2,39 @@
 
 let products =[`Laptop`, `Phone`, `Headphones`, `Monitor`];
 
+// calling the function to log the first product
 function logFirstProduct() {
   console.log(products[0]);
 }
 
+logFirstProduct();
+
+
 function addProduct(product) {
   products.push(product);
+  console.log(`Added product: ${product}`);
 }
+
+addProduct(`Keyboard`);
+
 
 function updateProductName(index, newName) {
   if (index >= 0 && index < products.length) {
     products[index] = newName;
   }
+  console.log(`Updated product at index ${index} to: ${newName}`);
 }
+
+updateProductName(1, `Smartphone`);
+
 
 function removeLastProduct() {
   products.pop();
+  console.log(`Removed the last product`);
 }
+removeLastProduct();
+
+console.log(products);
 
 // Export the necessary parts for testing
 module.exports = {
